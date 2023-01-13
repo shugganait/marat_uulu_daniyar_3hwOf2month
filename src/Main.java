17b33efc80b5227daf694d75a04c006c4ac22917
shugganait <daniarm209@gmail.com>
2023-01-13 18:07:01 +0600

    Исправил исключение на проверямое

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws LimitException {
         BankAccount account = new BankAccount();
         account.deposit(20000.0);
         boolean end = false;
@@ -9,11 +9,7 @@ public class Main {
                 System.out.println("Было снято, ваш текущий счет: " + account.getAmount());
             }catch (LimitException le){
                 System.out.println(le.getMessage());
-                try {
                     account.withDraw(le.getRemainingAmount());
-                } catch (LimitException e) {
-                    throw new RuntimeException(e);
-                }
                 System.out.println("Было снято отсавшая сумма, текущий счет: " + account.getAmount());
                 end = true;
             }
